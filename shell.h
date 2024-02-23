fe5977ad9de909e6506bee39bee44ae7a697accc
Salah Eldeen <mr.salahhashim@gmail.com>
2024-02-23 06:05:48 +0300

    shell_2

---
+++ shell.h
@@ -0,0 +1,20 @@
+/* string functions.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* string functions2.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* string functions.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char*, int);
+char *_strchr(char *, char);
+
+/* string functions.c */
+char **strtow(char*, char *);
+char **strtow2(char *, char);
